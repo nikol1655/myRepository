0dd610c2969a7cc592752374e07fa060f220c023
Michalska <nikol.michalska@gds.ey.com>
2023-03-31 12:50:10 +0200

    gradle support ex2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -5,14 +8,20 @@ import java.util.Scanner;
 
 public class Main {
 
+    static Logger logger = Logger.getLogger(Main.class);
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        BasicConfigurator.configure();
+        logger.warn("Application has started");
 
         long number = 0;
         String userAnswer = "";
+        logger.warn("Get sorted list from user");
         List<Long> numbers = getSortedListFromUser(number, userAnswer);
-        System.out.println("The 3rd decile is: " + countDecile(numbers, 3));
-        System.out.println("The 6th decile is: " + countDecile(numbers, 6));
+        logger.info("The 3rd decile is: " + countDecile(numbers, 3));
+        logger.info("The 6th decile is: " + countDecile(numbers, 6));
+
+        logger.warn("Application has finished");
 
     }
 
@@ -25,15 +34,15 @@ public class Main {
     private static List<Long> getSortedListFromUser(long number, String userAnswer) {
         List<Long> numbers = new ArrayList<>();
         do {
-            System.out.println("Type the number from 1 to 1000");
+            logger.info("Type the number from 1 to 1000");
             number = scanner.nextInt();
             if (number >= 1 && number <= 1000) {
                 numbers.add(number);
-                System.out.println("Type END if you want to finish");
+                logger.info("Type END if you want to finish");
                 scanner.nextLine();
                 userAnswer = scanner.nextLine();
             } else {
-                System.out.println("You have put the wrong number");
+                logger.info("You have put the wrong number");
             }
 
         } while (!userAnswer.equals("END"));
